ef7b0321e2ee988e8dedcb0d55afa5a309d9eba2
Makori Chacha <williammakori99@yahoo.com>
2024-03-12 18:47:56 -0600

    Second commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const containerEl = document.querySelector(".container");
+
+for (let index = 0; index < 30; index++) {
+    // adding this for loop will create 30 instances of boxes on the page 
+    const colorContainerEl = document.createElement("div")
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl);
+}
+
+function generateColors(){
+    colorContainerEl
+}
+function randomColor(){
+    const chars = "0123456789abcdef"
+    const colorCodeLength = 6;
+    let colorCode = ""
+    for (let index = 0; index < colorCodeLength; index++) {
+       const randomNum = Math.floor(Math.random() * chars.length);
+
+       console.log(randomNum);
+       colorCode += chars.substring(randomNum, randomNum + 1);
+       console.log(colorCode, randomNum);
+
+        
+    }
+}
